450d99521e7fc2ed6d63c698760c9ad26678a630
unknown <ramirobelfiore@gmail.com>
2024-03-11 21:00:12 -0300

    casi a la mitad del auth

---
+++ config/mails.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+
+function recuperacion($email, $token)
+{
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = '';
+        $mail->Password   = '';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+        $mail->setFrom('', 'Academia');
+        $mail->addAddress($email, $token);
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject =  'Recuperación de Cuenta';
+        $cuerpo = '<h4>Has solicitado una recuperación de la cuenta en Academia.</h4>';
+        $cuerpo .= '<h4>Sí ha realizado dicha solicitud, haga click en el siguiente enlace para restablecer su contraseña: <a style="text-decoration: none;" href="http://localhost/academia/reset_password.php?token=' . $token . '">Recuperar Cuenta</a></h4>';
+        $cuerpo .= '<h5>Sí no ha realizado esta solicitud, ignore este correo electrónico.</h5>';
+        $cuerpo .= '<p>Sí tenés otra duda/consulta, no dudes en consultarnos en <a style="text-decoration: none;" href="http://localhost/academia/contacto.php">Academia</a>, ó envianos un mail a este mismo correo: <b>academia@gmail.com</b></p>';
+        $mail->Body = utf8_decode($cuerpo);
+        $mail->AltBody = 'Recuperación de Cuenta';
+        $mail->setLanguage('es', '../PHPMailer/language/phpmailer.lang-es.php');
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Error al enviar el correo electrónico de la compra: {$mail->ErrorInfo}";
+        exit;
+    }
+}
---
+++ olvide.php
@@ -0,0 +1,34 @@
+<?php
+
+include("config/sesion.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = array();
+
+    $email = (isset($_POST["email"])) ? htmlspecialchars($_POST["email"]) : NULL;
+
+    if (empty($email)) $errors['email'] = "El E-Mail es obligatorio";
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['email'] = "El E-Mail tiene un formato inválido";
+
+    if (!empty($errors)) foreach ($errors as $error) echo "<br/>" . $error;
+    else {
+        $sql = "SELECT * FROM usuarios WHERE email= :email";
+
+        $result = $conn->prepare($sql);
+
+        $result->execute(array(':email' => $email));
+
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+
+        if ($result->rowCount() > 0) {
+            $token = bin2hex(random_bytes(32));
+
+            $update_token = $conn->prepare("UPDATE `usuarios` SET token = ? WHERE id = ?");
+
+            $update_token->execute([$token, $row['id']]);
+
+            recuperacion($email, $token);
+        }
+    }
+}
