229ed41aeae17fc1d3698bafac1cbc2ff3d75eae
unknown <ramirobelfiore@gmail.com>
2024-03-17 00:02:45 -0300

    comenzando con los roles

--- index.php
+++ index.php
@@ -15,6 +15,17 @@
     <?php if ($user_id != '') echo "<a href='update.php?id=$user_id'>Actualizar</a><br /><a href='logout.php'>Cerrar Sesión</a>" ?>
 
     <?php if ($user_id == '') echo "<a href='login.php'>Login</a><br /><a href='registro.php'>Registro</a>"; ?>
+
+    <?php
+    $select_teacher = $conn->prepare("SELECT * FROM `materias` WHERE instructor = ?");
+    $select_teacher->execute([$user_id]);
+
+    if ($select_teacher->rowCount() > 0) {
+        $fetch_teacher = $select_teacher->fetch(PDO::FETCH_ASSOC);
+
+        echo "<br /><a href='asistencia.php?id=" . $fetch_teacher['id'] . "'>Asistencia</a>";
+    }
+    ?>
 </body>
 
 </html>
